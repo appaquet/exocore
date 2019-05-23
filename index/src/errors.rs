3e81af6c7422cb9065e9dcf3c8f05eebff67e312
Andre-Philippe Paquet <appaquet@gmail.com>
2019-05-22 21:44:19 -0400

    Change structure for next PRs

---
+++ index/src/errors.rs
@@ -0,0 +1,29 @@
+#[derive(Debug, Fail)]
+pub enum Error {
+    #[fail(display = "An error as occurred in Query parsing: {:?}", _0)]
+    QueryParserError(tantivy::query::QueryParserError),
+    #[fail(display = "An error as occurred in Tantivy: {}", _0)]
+    TantivyError(tantivy::TantivyError),
+    #[fail(display = "A file error as occurred.")]
+    FileError,
+    #[fail(display = "An error occurred: {}", _0)]
+    Other(String),
+}
+
+impl From<tantivy::query::QueryParserError> for Error {
+    fn from(err: tantivy::query::QueryParserError) -> Self {
+        Error::QueryParserError(err)
+    }
+}
+
+impl From<tantivy::TantivyError> for Error {
+    fn from(err: tantivy::TantivyError) -> Self {
+        Error::TantivyError(err)
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(_: std::io::Error) -> Self {
+        Error::FileError
+    }
+}
--- index/src/lib.rs
+++ index/src/lib.rs
@@ -1,14 +1,11 @@
 #![deny(bare_trait_objects)]
 
-extern crate exocore_common;
-extern crate exocore_data;
-extern crate exocore_transport;
-
 #[macro_use]
 extern crate failure;
 
-extern crate tantivy;
-
 pub mod entity;
+pub mod errors;
+pub mod index;
+pub mod mutations;
 pub mod queries;
 pub mod store;
