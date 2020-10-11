eebf71ba6c6985451074dc8464d48f3b586894be
Andre-Philippe Paquet <appaquet@gmail.com>
2020-10-11 11:43:11 -0400

    feat(web_http): Expose http endpoints to web

--- examples/web/src/index.tsx
+++ examples/web/src/index.tsx
@@ -36,6 +36,7 @@ class App extends React.Component<IAppProps, IAppState> {
             return (
                 <div>
                     <button onClick={this.disconnect.bind(this)}>Reset</button>
+                    <div>{Exocore.store.httpEndpoints()}</div>
 
                     <List />
                 </div>
