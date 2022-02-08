095450b6cc410a0cc558eb18157294ec3ba845fa
Andre-Philippe Paquet <appaquet@gmail.com>
2022-02-08 07:39:31 -0500

    web: e2e tests

--- examples/web/playwright.config.ts
+++ examples/web/playwright.config.ts
@@ -9,14 +9,14 @@ const config: PlaywrightTestConfig = {
   testDir: './tests',
 
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 300 * 1000,
 
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 15000
+    timeout: 30000
   },
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
